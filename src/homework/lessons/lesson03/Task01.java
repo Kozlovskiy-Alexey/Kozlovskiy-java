77869a72971d65bdc6a0414307d98453da2f69c2
Kozlovskiy-Alexey <alexmapid83@gmail.com>
2020-11-22 17:48:45 +0300

    version 22.11.2020

--- src/homework/lessons/lesson03/Task01.java
+++ src/homework/lessons/lesson03/Task01.java
@@ -11,7 +11,9 @@ public class Task01 {
         int summ = 0;
         int a = 0;
         int[] array = new int[10];
+        int[] array2 = {3, 5, 9, 87, 45, 26, 48, 39, 78, 15};
 
+//задаем массив
         for (int i = 0; i < array.length; i++) {
             a = a + 5;
             array[i] = a;
@@ -27,6 +29,29 @@ public class Task01 {
         double mean = (double) summ / array.length;
         System.out.println("среднее значение суммы чисел массива = " + mean);
         System.out.println();
+
+        // сортировка массива array2 методом пузырька для нахождения медианы
+        //Arrays.sort(array2); сортировка методом
+        //System.out.println(Arrays.toString(array2));
+        boolean sort = false;
+        int temp;
+        while (!sort) {
+            sort = true;
+            for (int i = 0; i < array2.length - 1; i++) {
+                if (array2[i] > array2[i + 1]) {
+                    sort = false;
+
+                    temp = array2[i];
+                    array2[i] = array2[i + 1];
+                    array2[i + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array2));
+        // вычислим медиану, как полусумму средних значений
+        double med  = (double) (array2[4] + array2[5])/2;
+        System.out.println("the median is " + med);
+
     }
 
 }
